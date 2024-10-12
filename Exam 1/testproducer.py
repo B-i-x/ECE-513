ba403b0b42deeeca4fa40edbf765accdc2937e86
B-i-x <bix.romero@gmail.com>
2024-10-12 16:51:06 -0700

    chp3 is done

--- Exam 1/testproducer.py
+++ Exam 1/testproducer.py
@@ -1,10 +1,25 @@
 import os
 
 question_back = {}
+
+def parse_questions(filepath):
+    f = open(filepath, "r")
+    content = f.read()
+    print(content)
+    f.close()
+    return content
+
 d = 'sections'
 subsections = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
 
 for area in subsections:
     for subdir, dirs, files in os.walk(area):
         for file in files:
-            print(os.path.join(subdir, file))
+            filepath = os.path.join(subdir, file)
+            
+            if "questions" in file:
+                parse_questions(filepath)
+            elif "answers" in file:
+                pass
+
+            
