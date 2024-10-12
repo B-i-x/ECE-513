260f212860440e31c324aa59149700c4454f5252
B-i-x <bix.romero@gmail.com>
2024-10-12 16:15:50 -0700

    ocrd all of chp3

--- Exam 1/splitter.py
+++ Exam 1/splitter.py
@@ -7,7 +7,7 @@ def create_file(file_path):
 def split_sections(input_filepath):
 
     # Regex pattern to identify section headers e.g., "1.1 "
-    section_pattern = re.compile(r'^2\.\d+ [^\d]+$')
+    section_pattern = re.compile(r'^3\.\d+ [^\d]+$')
     
     # Initialize variables
     current_section = None
