2d7ae7e3e52920d8f232e8e916f6fb8355b4d930
B-i-x <bix.romero@gmail.com>
2024-11-04 16:20:25 -0700

    adding team act 11

---
+++ Team Activity 11/models/student.js
@@ -0,0 +1,20 @@
+const db = require("../db");
+
+
+/* your schema here */
+
+
+const Student = db.model("Student", studentSchema);
+
+
+
+module.exports = Student;
+
+
+// references
+// https://learn.zybooks.com/zybook/ARIZONAECE413SalehiFall2022/chapter/8/section/2
+// https://www.geeksforgeeks.org/mongoose-estimateddocumentcount-function/ 
+// https://www.geeksforgeeks.org/mongoose-find-function/
+// https://www.geeksforgeeks.org/mongoose-findoneandupdate-function/
+// https://www.geeksforgeeks.org/mongoose-deleteone-function/
+// https://docs.mongodb.com/manual/reference/operator/query/
---
+++ Team Activity 11/routes/students.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var Student = require("../models/student");
+
+// CRUD implementation
+
+
+
+
+module.exports = router;
