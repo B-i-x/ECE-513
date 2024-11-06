5fc8d08c1d189a88d78710664dbedd471381b2fd
B-i-x <bix.romero@gmail.com>
2024-11-05 20:59:07 -0700

    adding

--- Team Activity 11/models/student.js
+++ Team Activity 11/models/student.js
@@ -1,3 +1,4 @@
+const { router } = require("../app");
 const db = require("../db");
 
 
@@ -23,6 +24,37 @@ const Student = db.model("Student", studentSchema);
 module.exports = Student;
 
 
+function createStudent() {
+    if($('name').val()=== ""){
+        window.alert("iinvalid input");
+        return;
+    }
+    if($('#major').val() === ""){
+        window.alert("iinvalid input");
+        return;
+    }
+    let txdata = {
+        name: $('name').val(),
+        major: $('major').val(),
+        gpa: $('gpa').val()
+    }
+}
+
+$.ajax({
+    url: "/students/create",
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(txdata),
+    dataType: "json",
+})
+
+router.post("/create", function (req, res) {
+    const newStudent = new Student({
+        name: req.body.name,
+        major: req.body.major,
+        gpa: req.body.gpa,
+    });
+});
 // references
 // https://learn.zybooks.com/zybook/ARIZONAECE413SalehiFall2022/chapter/8/section/2
 // https://www.geeksforgeeks.org/mongoose-estimateddocumentcount-function/ 
